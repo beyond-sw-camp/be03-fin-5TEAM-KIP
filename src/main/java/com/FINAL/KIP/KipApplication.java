c9e45d623dfe9a6d58d4c0dd5cb01205c938ca21
GreatJang <1wkdwnsgur@gmail.com>
2024-03-20 12:48:55 +0900

    first_spring

---
+++ src/main/java/com/FINAL/KIP/KipApplication.java
@@ -0,0 +1,13 @@
+package com.FINAL.KIP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KipApplication.class, args);
+	}
+
+}
