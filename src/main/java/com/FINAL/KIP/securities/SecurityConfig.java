6df412d7a02af3bbd3e1df3ffa1147b3613bb0bf
GreatJang <1wkdwnsgur@gmail.com>
2024-03-27 11:52:37 +0900

    SecurityConfig_update

--- src/main/java/com/FINAL/KIP/securities/SecurityConfig.java
+++ src/main/java/com/FINAL/KIP/securities/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,45 +13,77 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 //토큰 설정
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true) // 권한관리 ROLE_ADMIN 관리자일 때 들어갈 수 있는 페이지 권한관리
-// pre : 사전, post : 사후, 사전/사후에 인증/권한 검사 어노테이션 사용가능
-public class SecurityConfig {
 
-    private final JwtAuthFilter authFilter;
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true) // 권한관리 ROLE_ADMIN 관리자일 때 들어갈 수 있는 페이지 권한관리
+@Configuration
+// pre : 사전, post : 사후, 사전/사후에 인증/권한 검사 어노테이션 사용가능
+public class SecurityConfig implements WebMvcConfigurer {
+
+    private final JwtAuthFilter jwtAuthFilter;
     @Autowired
-    public SecurityConfig(JwtAuthFilter authFilter) {
-        this.authFilter = authFilter;
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+//                    corsConfiguration.setAllowedOrigins(List.of("https://www.logeat.shop", "https://server.logeat.shop"));
+                    corsConfiguration.setAllowedMethods(List.of("GET","POST", "PUT","PATCH", "DELETE", "OPTIONS"));
+                    corsConfiguration.setAllowedHeaders(List.of("*"));
+                    corsConfiguration.addExposedHeader("New-Access-Token");
+                    return corsConfiguration;
+                }));
+        httpSecurity.httpBasic(basic -> basic.disable())
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers("/user/login", "/user").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, BasicAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Bean // Filter
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf ->
-                        csrf.disable()
-                ).cors(cors ->
-                        cors.disable()
-                )
-                .authorizeHttpRequests(request -> request
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .requestMatchers("/user/login", "/user").permitAll()
-                        .anyRequest().authenticated()
 
-                )
-
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .addFilterBefore(authFilter, BasicAuthenticationFilter.class);
-
-        return http.build();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
+//
+//    @Bean // Filter
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf(csrf ->
+//                        csrf.disable()
+//                ).cors(cors ->
+//                        cors.disable()
+//                )
+//                .authorizeHttpRequests(request -> request
+//                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+//                        .requestMatchers("/user/login", "/user").permitAll()
+//                        .anyRequest().authenticated()
+//
+//                )
+//
+//                .sessionManagement(session ->
+//                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                )
+//                .addFilterBefore(authFilter, BasicAuthenticationFilter.class);
+//
+//        return http.build();
+//    }
 }
