617a860e7074f1ad7c701537dc4df0c3b870e8fb
GreatJang <1wkdwnsgur@gmail.com>
2024-04-02 17:15:29 +0900

    [feat]Logout

--- src/main/java/com/FINAL/KIP/user/service/UserService.java
+++ src/main/java/com/FINAL/KIP/user/service/UserService.java
@@ -104,6 +104,21 @@ public class UserService {
     }
 
     @UserAdmin
+    public CommonResponse logout(Long id) throws IllegalArgumentException{
+        User user = userRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("없는 사용자입니다."));
+
+        UserRefreshToken userRefreshToken = userRefreshTokenRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 다시 확인해주세요."));
+
+        userRefreshTokenRepository.deleteById(id);
+        Map<String , String> result = new HashMap<>();
+        result.put("user_id", String.valueOf(userRefreshToken.getId()));
+        result.put("user_name", user.getName());
+        return new CommonResponse(HttpStatus.OK, "User Logout SUCCESS!", id);
+    }
+
+    @UserAdmin
     public CommonResponse mypage(){
         User userInfo = getUserFromAuthentication();
         return new CommonResponse(HttpStatus.OK, "User info loaded successfully!", userInfo);
