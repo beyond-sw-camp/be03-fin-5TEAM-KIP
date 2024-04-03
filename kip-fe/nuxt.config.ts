9a612f2ff75ac90d57e2398f93c6583b6ee6498b
SejongPark <lifedesigner88@gmail.com>
2024-04-03 14:44:36 +0900

    [Front] 다크모드 & 아이콘 설정
    https://heroicons.com/ 아이콘 사이트

--- kip-fe/nuxt.config.ts
+++ kip-fe/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true}, // https://devtools.nuxt.com/
-    modules: ['@nuxt/ui'] // Using package name
+    modules: ['@nuxt/ui'], // Nuxt 불러오는 설정
+    ui: {
+        global: true, // 컴포넌트 전역적으로 사용
+    },
 })
