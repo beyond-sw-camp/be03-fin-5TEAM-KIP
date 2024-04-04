79d8c7fe53a73e9d8062cb12945e5aba4d4345d0
SejongPark <lifedesigner88@gmail.com>
2024-04-04 09:08:49 +0900

    [Front] 코드 살짝 정리 + 주석

--- kip-fe/layouts/default.vue
+++ kip-fe/layouts/default.vue
@@ -5,6 +5,7 @@ const drawer = ref(true);
 
 <template>
   <v-layout class="rounded rounded-md">
+
     <!--  상단메뉴  -->
     <v-app-bar
         location="top"
@@ -12,20 +13,25 @@ const drawer = ref(true);
         :color="`#2D3250`"
         :elevation="5"
     >
+
       <template #prepend>
+        <!-- 햄버거 버튼 -->
         <v-app-bar-nav-icon
             variant="text"
             @click.stop="$event => drawer = !drawer"
         />
+      </template>
+
         <v-toolbar-title>
           <NuxtLink to="/">
             KIP (Knowledge Is Power)
           </NuxtLink>
         </v-toolbar-title>
-      </template>
+
       <template #append>
         <v-menu>
           <template v-slot:activator="{ props }">
+            <!-- 아바타 버튼 -->
             <v-avatar
                 image="https://avatars.githubusercontent.com/u/123573918?v=4"
                 size="55"
@@ -44,11 +50,10 @@ const drawer = ref(true);
           </v-list>
         </v-menu>
       </template>
-
     </v-app-bar>
 
 
-    <!--  우측메뉴  -->
+    <!--  좌측메뉴  -->
     <v-navigation-drawer
         expand-on-hover
         rail
@@ -71,7 +76,7 @@ const drawer = ref(true);
     <!--  메인 페이지  -->
     <v-main>
       <div class="pa-5">
-      <NuxtPage/>
+        <NuxtPage/>
       </div>
 
     </v-main>
