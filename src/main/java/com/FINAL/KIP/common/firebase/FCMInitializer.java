24bf36b224e3433ca91bde2437759f21a3ee839a
GreatJang <1wkdwnsgur@gmail.com>
2024-05-01 11:56:14 +0900

    [fix] No FCM POD3

--- src/main/java/com/FINAL/KIP/common/firebase/FCMInitializer.java
+++ src/main/java/com/FINAL/KIP/common/firebase/FCMInitializer.java
@@ -1,34 +1,35 @@
-//package com.FINAL.KIP.common.firebase;
-//
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
-//import jakarta.annotation.PostConstruct;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.core.io.ClassPathResource;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class FCMInitializer {
-//
-//	@Value("${fcm.certification}")
-//	private String googleApplicationCredentials;
-//
-//	@PostConstruct
-//	public void initialize() throws IOException {
-//		ClassPathResource resource = new ClassPathResource(googleApplicationCredentials);
-//
-//		try (InputStream is = resource.getInputStream()) {
-//			FirebaseOptions options = FirebaseOptions.builder()
-//				.setCredentials(GoogleCredentials.fromStream(is))
-//				.build();
-//
-//			if (FirebaseApp.getApps().isEmpty()) {
-//				FirebaseApp.initializeApp(options);
-//			}
-//		}
-//	}
-//
-//}
+package com.FINAL.KIP.common.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class FCMInitializer {
+
+	@Value("${fcm.certification}")
+	private String googleApplicationCredentials;
+
+	@PostConstruct
+	public void initialize() throws IOException {
+		ClassPathResource resource = new ClassPathResource(googleApplicationCredentials);
+
+		try (InputStream is = resource.getInputStream()) {
+			FirebaseOptions options = FirebaseOptions.builder()
+				.setCredentials(GoogleCredentials.fromStream(is))
+				.build();
+
+			if (FirebaseApp.getApps().isEmpty()) {
+				FirebaseApp.initializeApp(options);
+			}
+		}
+	}
+
+}
